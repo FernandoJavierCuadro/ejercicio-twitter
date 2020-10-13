df6f25ddd3adcd6d4ab33801feb32221fc87d0b3
nandopeyton <68226901+nandopeyton@users.noreply.github.com>
2020-10-13 11:55:45 -0300

    initial commit

---
+++ server.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+const session = require("express-session");
+const passport = require("passport");
+const routes = require("./routes/routes");
+
+app.use(express.json());
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(express.urlencoded({ extended: false }));
+
+app.use(
+  session({
+    secret: "AlgúnTextoSuperSecreto",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+routes(app);
+
+app.listen(3000, () => {
+    console.log("servidor escuchando en el puerto 3000...");
+    console.log("ingresar a http://localhost:3000");
+  });
