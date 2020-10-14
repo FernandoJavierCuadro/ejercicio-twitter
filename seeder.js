c657993803d918b871c997c60c8673e6c5f01e1a
nandopeyton <68226901+nandopeyton@users.noreply.github.com>
2020-10-14 11:39:46 -0300

    fixed tweetsseed

--- seeder.js
+++ seeder.js
@@ -16,12 +16,13 @@ const usersSeed = (mongoose, User) => {
 };
 
 const tweetsSeed = (mongoose, Tweet) => {
-  /* for (i = 0; i < 20; i++) {
+  for (i = 0; i < 20; i++) {
     const tweet = new Tweet({
       text: faker.lorem.words(),
+      author: "5f860ac2dc5d06548c273ddd",
     });
     tweet.save();
-  } */
+  }
 };
 
 module.exports = { usersSeed, tweetsSeed };
